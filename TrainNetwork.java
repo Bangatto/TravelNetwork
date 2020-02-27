09481ced640333eb0e8e44afc017112e7bd89f15
Gattuoch Kuon <kuon2018@outlook.com>
2020-02-26 21:01:35 -0500

    Made last changes to train Network

--- TrainNetwork.java
+++ TrainNetwork.java
@@ -13,54 +13,73 @@ public class TrainNetwork {
     public TrainLine[] getLines() {
     	return this.networkLines;
     }
-    
+  
     public void dance() {
     	System.out.println("The tracks are moving!");
-    	//YOUR CODE GOES HERE
+    	//loop through the train line array and 
+    	TrainLine[] lines = getLines();
+    	for(TrainLine line: lines) {
+    		line.shuffleLine();
+    	}
+    	
     }
     
     public void undance() {
-    	//YOUR CODE GOES HERE
+    	//loop through each trainLin, sort each line
+    	TrainLine[] lines = getLines();
+    	for(TrainLine line: lines) {
+    		line.sortLine();
+    	}
     }
     
     public int travel(String startStation, String startLine, String endStation, String endLine) {
     	
-    	TrainLine curLine = null; //use this variable to store the current line.
-    	TrainStation curStation= null; //use this variable to store the current station. 
-    	
+    	TrainLine curLine = getLineByName(startLine); //use this variable to store the current line.
+    	TrainStation curStation= curLine.findStation(startStation); //use this variable to store the current station. 
+    	TrainLine endTrainLine = getLineByName(endLine);
+    	TrainStation endTrainStation = endTrainLine.findStation(endStation);
+    	TrainStation prevStation = curStation;
     	
     	int hoursCount = 0;
     	System.out.println("Departing from "+startStation);
-    	
-    	//YOUR CODE GOES HERE
-    	
-    	
-    	while(true /*you can change this*/) {
-    		
-    		if(hoursCount == 168) {
-    			System.out.println("Jumped off after spending a full week on the train. Might as well walk.");
-    			return hoursCount;
+    	while(!curStation.equals(endTrainStation)) {
+    		TrainStation nextStation = curLine.travelOneStation(curStation, prevStation);
+    		prevStation = curStation;
+    		curStation = nextStation;
+    		curLine =curStation.getLine(); 
+    		hoursCount++;
+    		//check whether 2hrs passed and dance the lines every two hours.
+    		if(hoursCount %2 ==0) {
+    			this.dance();
     		}
-    		
+    		if(hoursCount == 168) {
+				System.out.println("Jumped off after spending a full week on the train. Might as well walk.");
+				return hoursCount;
+    		}
     		//prints an update on your current location in the network.
 	    	System.out.println("Traveling on line "+curLine.getName()+":"+curLine.toString());
 	    	System.out.println("Hour "+hoursCount+". Current station: "+curStation.getName()+" on line "+curLine.getName());
 	    	System.out.println("=============================================");
 	    	
-	    	break; //remove this! this break is only there so the template compiles.
-    		}
-	    	
-	    	System.out.println("Arrived at destination after "+hoursCount+" hours!");
-	    	return hoursCount;
+    	}
+	    System.out.println("Arrived at destination after "+hoursCount+" hours!");
+	    return hoursCount;
     }
-    
     
     //you can extend the method header if needed to include an exception. You cannot make any other change to the header.
     public TrainLine getLineByName(String lineName){
-    	//YOUR CODE GOES HERE
-    	
-    	return null; //change this
+    	//loop through the network lines
+    	//check if the current equals the line name and if not throw an error
+    	TrainLine[] lines = getLines();
+    	for (TrainLine curLine: lines) {
+    		if (curLine.getName().equals(lineName)) {
+    			return curLine;
+    			}
+    	}
+    	throw new LineNotFoundException("this line is not in this network");
+   
     }
+    
     
   //prints a plan of the network for you.
     public void printPlan() {
